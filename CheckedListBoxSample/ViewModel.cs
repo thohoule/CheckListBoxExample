091c8a2d60ee8b202c65037e682056e48558d390
thohoule <116042865+thohoule@users.noreply.github.com>
2022-10-17 18:49:08 -0500

    Add files via upload

---
+++ CheckedListBoxSample/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckedListBoxSample
+{
+  public class ViewModel
+    {
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>
+        {
+            "AAAAA", "BBBBB", "CCCCC", "DDDDD", "EEEEE"
+        };
+    }
+}
